c5e8c2c452519fccba2cd114fc35b74abb450d0a
Dong Liu <edongliu@gmail.com>
2013-10-02 16:08:13 -0400

    clean table code, manage approving reject table ready

--- public/javascripts/manage.js
+++ public/javascripts/manage.js
@@ -1,163 +1,27 @@
 var approvingTableColumns = {
-  from: [12, 13, 14, 15, 16, 17, 18, 19],
-  to: [20, 21, 22, 23, 24, 25, 26, 27],
-  comments: [28]
+  from: [13, 14, 15, 16, 17, 18, 19, 20],
+  to: [21, 22, 23, 24, 25, 26, 27, 28],
+  comments: [29]
 };
 
 var rejectedTableColumns = approvingTableColumns;
 
 $(function() {
   /*approving table starts*/
+
+  var approvingAoCulumns = [selectColumn, editLinkColumn, submittedOnColumn, updatedOnColumn, submittedByColumn].concat(basicColumns, fromColumns, toColumns).concat([commentsColumn]);
+  fnAddFilterFoot('#approving-table', approvingAoCulumns);
   var approvingTable = $('#approving-table').dataTable({
     aaData: [],
     bAutoWidth: false,
-    aoColumns: [{
-      sTitle: '',
-      sDefaultContent: '<label class="checkbox"><input type="checkbox" class="select-row"></label>',
-      sSortDataType: 'dom-checkbox',
-      asSorting: ['desc', 'asc']
-    }, {
-      sTitle: '',
-      mData: '_id',
-      mRender: function(data, type, full) {
-        return '<a href="requests/' + data + '" target="_blank"><i class="icon-file-text icon-large"></i></a>';
-      },
-      bSortable: false
-    }, {
-      sTitle: 'Submitted on',
-      mData: 'submittedOn',
-      mRender: function(data, type, full) {
-        return formatDate(data);
-      }
-    }, {
-      sTitle: 'Updated on',
-      sDefaultContent: '',
-      mData: 'updatedOn',
-      mRender: function(data, type, full) {
-        return formatDate(data);
-      }
-    }, {
-      sTitle: 'Submitted by',
-      mData: 'submittedBy',
-      mRender: function(data, type, full) {
-        return '<a href = "/users/' + data + '" target="_blank">' + data + '</a>';
-      }
-    }, {
-      sTitle: 'project',
-      sDefaultContent: '',
-      mData: 'basic.project'
-    }, {
-      sTitle: 'SSS',
-      sDefaultContent: '',
-      mData: function(source, type, val) {
-        return (source.basic.system ? source.basic.system : '?') + (source.basic.subsystem ? source.basic.subsystem : '?') + (source.basic.signal ? source.basic.signal : '?');
-      }
-    }, {
-      sTitle: 'Cable type',
-      sDefaultContent: '',
-      mData: 'basic.cableType'
-    }, {
-      sTitle: 'Engineer',
-      sDefaultContent: '',
-      mData: 'basic.engineer'
-    }, {
-      sTitle: 'Service',
-      sDefaultContent: '',
-      mData: 'basic.service'
-    }, {
-      sTitle: 'WBS',
-      sDefaultContent: '',
-      mData: 'basic.wbs'
-    }, {
-      sTitle: 'Quantity',
-      sDefaultContent: '',
-      mData: 'basic.quantity'
-    }, {
-      sTitle: 'From building',
-      sDefaultContent: '',
-      mData: 'from.building'
-    }, {
-      sTitle: 'room',
-      sDefaultContent: '',
-      mData: 'from.room'
-    }, {
-      sTitle: 'elevation',
-      sDefaultContent: '',
-      mData: 'from.elevation'
-    }, {
-      sTitle: 'unit',
-      sDefaultContent: '',
-      mData: 'from.unit'
-    }, {
-      sTitle: 'term. device',
-      sDefaultContent: '',
-      mData: 'from.terminationDevice'
-    }, {
-      sTitle: 'term. type',
-      sDefaultContent: '',
-      mData: 'from.terminationType'
-    }, {
-      sTitle: 'wiring drawing',
-      sDefaultContent: '',
-      mData: 'from.wiringDrawing'
-    }, {
-      sTitle: 'lebel',
-      sDefaultContent: '',
-      mData: 'from.label'
-    }, {
-      sTitle: 'To building',
-      sDefaultContent: '',
-      mData: 'to.building'
-    }, {
-      sTitle: 'room',
-      sDefaultContent: '',
-      mData: 'to.room'
-    }, {
-      sTitle: 'elevation',
-      sDefaultContent: '',
-      mData: 'to.elevation'
-    }, {
-      sTitle: 'unit',
-      sDefaultContent: '',
-      mData: 'to.unit'
-    }, {
-      sTitle: 'term. device',
-      sDefaultContent: '',
-      mData: 'to.terminationDevice'
-    }, {
-      sTitle: 'term. type',
-      sDefaultContent: '',
-      mData: 'to.terminationType'
-    }, {
-      sTitle: 'wiring drawing',
-      sDefaultContent: '',
-      mData: 'to.wiringDrawing'
-    }, {
-      sTitle: 'lebel',
-      sDefaultContent: '',
-      mData: 'to.label'
-    }, {
-      sTitle: 'Comments',
-      sDefaultContent: '',
-      mData: 'comments'
-    }],
-    'aaSorting': [
+    aoColumns: approvingAoCulumns,
+    aaSorting: [
       [2, 'desc'],
       [3, 'desc'],
       [4, 'desc']
     ],
-    "sDom": "<'row-fluid'<'span6'<'control-group'T>>><'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-    "oTableTools": {
-      "sSwfPath": "datatables/swf/copy_csv_xls_pdf.swf",
-      "aButtons": [
-        "copy",
-        "print", {
-          "sExtends": "collection",
-          "sButtonText": 'Save <span class="caret" />',
-          "aButtons": ["csv", "xls", "pdf"]
-        }
-      ]
-    }
+    sDom: sDom,
+    oTableTools: oTableTools
   });
 
   $('#approving-wrap').click(function(e) {
@@ -176,67 +40,18 @@ $(function() {
 
   $('#approving-select-none').click(function(e) {
     fnDeselect(approvingTable, 'row-selected', 'select-row');
-    approvingTable.fnAdjustColumnSizing();
+  });
+
+  $('#approving-select-all').click(function(e) {
+    fnSelectAll(approvingTable, 'row-selected', 'select-row', true);
   });
 
   $('#approving-approve').click(function(e) {
-    var selected = fnGetSelected(approvingTable, 'row-selected');
-    var requests = {};
-    if (selected.length) {
-      $('#modalLable').html('Approve the following ' + selected.length + ' requests? ');
-      $('#modal .modal-body').empty();
-      selected.forEach(function(row) {
-        var data = approvingTable.fnGetData(row);
-        $('#modal .modal-body').append('<div id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.system + data.basic.subsystem + data.basic.signal + '||' + data.basic.wbs + '</div>');
-        requests[data._id] = {
-          basic: data.basic,
-          from: data.from,
-          to: data.to,
-          comments: data.comments
-        };
-      });
-      // $('#modal .modal-body').html('test');
-      $('#modal .modal-footer').html('<button id="approve" class="btn btn-primary">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
-      $('#modal').modal('show');
-      $('#approve').click(function(e) {
-        approveFromModal(requests, approvingTable);
-      });
-    } else {
-      $('#modalLable').html('Alert');
-      $('#modal .modal-body').html('No request has been selected!');
-      $('#modal .modal-footer').html('<button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
-      $('#modal').modal('show');
-    }
+    batchApprove(approvingTable);
   });
 
   $('#approving-reject').click(function(e) {
-    var selected = fnGetSelected(approvingTable, 'row-selected');
-    var requests = {};
-    if (selected.length) {
-      $('#modalLable').html('Reject the following ' + selected.length + ' requests? ');
-      $('#modal .modal-body').empty();
-      selected.forEach(function(row) {
-        var data = approvingTable.fnGetData(row);
-        $('#modal .modal-body').append('<div id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.system + data.basic.subsystem + data.basic.signal + '||' + data.basic.wbs + '</div>');
-        requests[data._id] = {
-          basic: data.basic,
-          from: data.from,
-          to: data.to,
-          comments: data.comments
-        };
-      });
-      // $('#modal .modal-body').html('test');
-      $('#modal .modal-footer').html('<button id="reject" class="btn btn-primary">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
-      $('#modal').modal('show');
-      $('#reject').click(function(e) {
-        rejectFromModal(requests, approvingTable);
-      });
-    } else {
-      $('#modalLable').html('Alert');
-      $('#modal .modal-body').html('No request has been selected!');
-      $('#modal .modal-footer').html('<button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
-      $('#modal').modal('show');
-    }
+    batchReject(approvingTable);
   });
 
 
@@ -246,168 +61,22 @@ $(function() {
 
   /*rejected tab starts*/
 
+  var rejectedAoColumns = [detailsLinkColumn, rejectedOnColumn, submittedOnColumn, submittedByColumn].concat(basicColumns, fromColumns, toColumns).concat([commentsColumn]);
+  fnAddFilterFoot('#rejected-table', rejectedAoColumns);
   var rejectedTable = $('#rejected-table').dataTable({
     aaData: [],
     bAutoWidth: false,
-    aoColumns: [{
-      sTitle: '',
-      sDefaultContent: '<label class="checkbox"><input type="checkbox" class="select-row"></label>',
-      sSortDataType: 'dom-checkbox',
-      asSorting: ['desc', 'asc']
-    }, {
-      sTitle: '',
-      mData: '_id',
-      mRender: function(data, type, full) {
-        return '<a href="requests/' + data + '" target="_blank"><i class="icon-file-text icon-large"></i></a>';
-      },
-      bSortable: false
-    }, {
-      sTitle: 'Rejected on',
-      mData: 'rejectedOn',
-      mRender: function(data, type, full) {
-        return formatDate(data);
-      }
-    }, {
-      sTitle: 'Submitted on',
-      mData: 'submittedOn',
-      mRender: function(data, type, full) {
-        return formatDate(data);
-      }
-    }, {
-      sTitle: 'Submitted by',
-      mData: 'submittedBy',
-      mRender: function(data, type, full) {
-        return '<a href = "/users/' + data + '" target="_blank">' + data + '</a>';
-      }
-    }, {
-      sTitle: 'project',
-      sDefaultContent: '',
-      mData: 'basic.project'
-    }, {
-      sTitle: 'SSS',
-      sDefaultContent: '',
-      mData: function(source, type, val) {
-        return (source.basic.system ? source.basic.system : '?') + (source.basic.subsystem ? source.basic.subsystem : '?') + (source.basic.signal ? source.basic.signal : '?');
-      }
-    }, {
-      sTitle: 'Cable type',
-      sDefaultContent: '',
-      mData: 'basic.cableType'
-    }, {
-      sTitle: 'Engineer',
-      sDefaultContent: '',
-      mData: 'basic.engineer'
-    }, {
-      sTitle: 'Service',
-      sDefaultContent: '',
-      mData: 'basic.service'
-    }, {
-      sTitle: 'WBS',
-      sDefaultContent: '',
-      mData: 'basic.wbs'
-    }, {
-      sTitle: 'Quantity',
-      sDefaultContent: '',
-      mData: 'basic.quantity'
-    }, {
-      sTitle: 'From building',
-      sDefaultContent: '',
-      mData: 'from.building'
-    }, {
-      sTitle: 'room',
-      sDefaultContent: '',
-      mData: 'from.room'
-    }, {
-      sTitle: 'elevation',
-      sDefaultContent: '',
-      mData: 'from.elevation'
-    }, {
-      sTitle: 'unit',
-      sDefaultContent: '',
-      mData: 'from.unit'
-    }, {
-      sTitle: 'term. device',
-      sDefaultContent: '',
-      mData: 'from.terminationDevice'
-    }, {
-      sTitle: 'term. type',
-      sDefaultContent: '',
-      mData: 'from.terminationType'
-    }, {
-      sTitle: 'wiring drawing',
-      sDefaultContent: '',
-      mData: 'from.wiringDrawing'
-    }, {
-      sTitle: 'lebel',
-      sDefaultContent: '',
-      mData: 'from.label'
-    }, {
-      sTitle: 'To building',
-      sDefaultContent: '',
-      mData: 'to.building'
-    }, {
-      sTitle: 'room',
-      sDefaultContent: '',
-      mData: 'to.room'
-    }, {
-      sTitle: 'elevation',
-      sDefaultContent: '',
-      mData: 'to.elevation'
-    }, {
-      sTitle: 'unit',
-      sDefaultContent: '',
-      mData: 'to.unit'
-    }, {
-      sTitle: 'term. device',
-      sDefaultContent: '',
-      mData: 'to.terminationDevice'
-    }, {
-      sTitle: 'term. type',
-      sDefaultContent: '',
-      mData: 'to.terminationType'
-    }, {
-      sTitle: 'wiring drawing',
-      sDefaultContent: '',
-      mData: 'to.wiringDrawing'
-    }, {
-      sTitle: 'lebel',
-      sDefaultContent: '',
-      mData: 'to.label'
-    }, {
-      sTitle: 'Comments',
-      sDefaultContent: '',
-      mData: 'comments'
-    }],
-    'aaSorting': [
+    aoColumns: rejectedAoColumns,
+    aaSorting: [
       [2, 'desc'],
       [3, 'desc'],
       [4, 'desc']
     ],
-    "sDom": "<'row-fluid'<'span6'<'control-group'T>>><'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-    "oTableTools": {
-      "sSwfPath": "datatables/swf/copy_csv_xls_pdf.swf",
-      "aButtons": [
-        "copy",
-        "print", {
-          "sExtends": "collection",
-          "sButtonText": 'Save <span class="caret" />',
-          "aButtons": ["csv", "xls", "pdf"]
-        }
-      ]
-    }
+    sDom: sDom,
+    oTableTools: oTableTools
   });
-  $.ajax({
-    url: '/requests/statuses/3/json',
-    type: 'GET',
-    dataType: 'json'
-  }).done(function(json) {
-    rejectedTable.fnClearTable();
-    rejectedTable.fnAddData(json);
-    rejectedTable.fnDraw();
-  }).fail(function(jqXHR, status, error) {
-    $('#message').append('<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>Cannot reach the server for cable requests.</div>');
-    $(window).scrollTop($('#message div:last-child').offset().top - 40);
-  }).always();
+
+  initRequestTable(rejectedTable, 'requests/statuses/3/json');
 
   $('#rejected-wrap').click(function(e) {
     $('#rejected-table td').removeClass('nowrap');
@@ -419,9 +88,9 @@ $(function() {
     rejectedTable.fnAdjustColumnSizing();
   });
 
-  $('#rejected-show input:checkbox').change(function(e) {
-    fnSetColumnsVis(rejectedTable, rejectedTableColumns[$(this).val()], $(this).prop('checked'));
-  });
+  // $('#rejected-show input:checkbox').change(function(e) {
+  //   fnSetColumnsVis(rejectedTable, rejectedTableColumns[$(this).val()], $(this).prop('checked'));
+  // });
 
 
   var procuring = [];
@@ -478,13 +147,7 @@ $(function() {
   }).always();
 
   /*all tabs*/
-  $('tbody').on('click', 'input.select-row', function(e) {
-    if ($(this).prop('checked')) {
-      $(e.target).closest('tr').addClass('row-selected');
-    } else {
-      $(e.target).closest('tr').removeClass('row-selected');
-    }
-  });
+  addEvents();
 
 });
 
@@ -498,12 +161,44 @@ function initRequestTable(table, url) {
     table.fnClearTable();
     table.fnAddData(json);
     table.fnDraw();
-    // addClick($('#approving-table'), approvingTable, 7);
   }).fail(function(jqXHR, status, error) {
     $('#message').append('<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>Cannot reach the server for cable requests.</div>');
     $(window).scrollTop($('#message div:last-child').offset().top - 40);
   }).always();
 }
+
+// function
+
+function batchApprove(oTable) {
+  var selected = fnGetSelected(oTable, 'row-selected');
+  var requests = {};
+  if (selected.length) {
+    $('#modalLable').html('Approve the following ' + selected.length + ' requests? ');
+    $('#modal .modal-body').empty();
+    selected.forEach(function(row) {
+      var data = oTable.fnGetData(row);
+      $('#modal .modal-body').append('<div id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.system + data.basic.subsystem + data.basic.signal + '||' + data.basic.wbs + '</div>');
+      requests[data._id] = {
+        basic: data.basic,
+        from: data.from,
+        to: data.to,
+        comments: data.comments
+      };
+    });
+    $('#modal .modal-footer').html('<button id="approve" class="btn btn-primary">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
+    $('#modal').modal('show');
+    $('#approve').click(function(e) {
+      approveFromModal(requests, oTable);
+    });
+  } else {
+    $('#modalLable').html('Alert');
+    $('#modal .modal-body').html('No request has been selected!');
+    $('#modal .modal-footer').html('<button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
+    $('#modal').modal('show');
+  }
+}
+
+
 
 function approveFromModal(requests, approvingTable) {
   $('#approve').prop('disabled', true);
@@ -536,6 +231,36 @@ function approveFromModal(requests, approvingTable) {
   });
 }
 
+function batchReject(oTable) {
+  var selected = fnGetSelected(oTable, 'row-selected');
+  var requests = {};
+  if (selected.length) {
+    $('#modalLable').html('Reject the following ' + selected.length + ' requests? ');
+    $('#modal .modal-body').empty();
+    selected.forEach(function(row) {
+      var data = oTable.fnGetData(row);
+      $('#modal .modal-body').append('<div id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.system + data.basic.subsystem + data.basic.signal + '||' + data.basic.wbs + '</div>');
+      requests[data._id] = {
+        basic: data.basic,
+        from: data.from,
+        to: data.to,
+        comments: data.comments
+      };
+    });
+    // $('#modal .modal-body').html('test');
+    $('#modal .modal-footer').html('<button id="reject" class="btn btn-primary">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
+    $('#modal').modal('show');
+    $('#reject').click(function(e) {
+      rejectFromModal(requests, oTable);
+    });
+  } else {
+    $('#modalLable').html('Alert');
+    $('#modal .modal-body').html('No request has been selected!');
+    $('#modal .modal-footer').html('<button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
+    $('#modal').modal('show');
+  }
+}
+
 function rejectFromModal(requests, approvingTable) {
   $('#reject').prop('disabled', true);
   var number = $('#modal .modal-body div').length;
@@ -562,6 +287,7 @@ function rejectFromModal(requests, approvingTable) {
         number = number - 1;
         if (number === 0) {
           initRequestTable(approvingTable, '/requests/statuses/1/json');
+          initRequestTable(rejectedTable, 'requests/statuses/3/json');
         }
       });
   });
--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -1,15 +1,293 @@
-function fnWrap(oTableLocal){
-  $(oTableLocal.fnSettings().aoData).each(function(){
-      $(this.nTr).removeClass('nowrap');
-    });
-    oTableLocal.fnAdjustColumnSizing();
+// global cable variables
+
+var selectColumn = {
+  sTitle: '',
+  sDefaultContent: '<label class="checkbox"><input type="checkbox" class="select-row"></label>',
+  sSortDataType: 'dom-checkbox',
+  asSorting: ['desc', 'asc']
+};
+
+var editLinkColumn = {
+  sTitle: '',
+  mData: '_id',
+  mRender: function(data, type, full) {
+    return '<a href="requests/' + data + '" target="_blank"><i class="icon-edit icon-large"></i></a>';
+  },
+  bSortable: false
+};
+
+var detailsLinkColumn = {
+  sTitle: '',
+  mData: '_id',
+  mRender: function(data, type, full) {
+    return '<a href="requests/' + data + '/details" target="_blank"><i class="icon-file-text-alt icon-large"></i></a>';
+  },
+  bSortable: false
+};
+
+var createdOnColumn = dateColumn('Created on', 'createdOn');
+
+var updatedOnColumn = dateColumn('Updated on', 'updatedOn');
+
+var submittedOnColumn = dateColumn('Submitted on', 'submittedOn');
+var submittedByColumn = personColumn('Submitted by', 'submittedBy');
+
+var approvedOnColumn = dateColumn('Approved on', 'approvedOn');
+var approvedByColumn = personColumn('Approved by', 'approvedBy');
+
+var rejectedOnColumn = dateColumn('Rejected on', 'rejectedOn');
+var rejectedByColumn = personColumn('Rejected by', 'rejectedBy');
+
+var commentsColumn = {
+  sTitle: 'Comments',
+  sDefaultContent: '',
+  mData: 'comments',
+  bFilter: true
+};
+
+var basicColumns = [{
+  sTitle: 'project',
+  sDefaultContent: '',
+  mData: 'basic.project',
+  bFilter: true
+}, {
+  sTitle: 'SSS',
+  sDefaultContent: '',
+  mData: function(source, type, val) {
+    return (source.basic.system ? source.basic.system : '?') + (source.basic.subsystem ? source.basic.subsystem : '?') + (source.basic.signal ? source.basic.signal : '?');
+  },
+  bFilter: true
+}, {
+  sTitle: 'Cable type',
+  sDefaultContent: '',
+  mData: 'basic.cableType',
+  bFilter: true
+}, {
+  sTitle: 'Engineer',
+  sDefaultContent: '',
+  mData: 'basic.engineer',
+  bFilter: true
+}, {
+  sTitle: 'Service',
+  sDefaultContent: '',
+  mData: 'basic.service',
+  bFilter: true
+}, {
+  sTitle: 'WBS',
+  sDefaultContent: '',
+  mData: 'basic.wbs',
+  bFilter: true
+}, {
+  sTitle: 'Tags',
+  sDefaultContent: '',
+  mData: function(source, type, val) {
+    if (source.basic.tags) {
+      return source.basic.tags.join();
+    } else {
+      return '';
+    }
+  },
+  bFilter: true
+}, {
+  sTitle: 'Quantity',
+  sDefaultContent: '',
+  mData: 'basic.quantity',
+  bFilter: true
+}];
+var fromColumns = [{
+  sTitle: 'From building',
+  sDefaultContent: '',
+  mData: 'from.building',
+  bFilter: true
+}, {
+  sTitle: 'room',
+  sDefaultContent: '',
+  mData: 'from.room',
+  bFilter: true
+}, {
+  sTitle: 'elevation',
+  sDefaultContent: '',
+  mData: 'from.elevation',
+  bFilter: true
+}, {
+  sTitle: 'unit',
+  sDefaultContent: '',
+  mData: 'from.unit',
+  bFilter: true
+}, {
+  sTitle: 'term. device',
+  sDefaultContent: '',
+  mData: 'from.terminationDevice',
+  bFilter: true
+}, {
+  sTitle: 'term. type',
+  sDefaultContent: '',
+  mData: 'from.terminationType',
+  bFilter: true
+}, {
+  sTitle: 'wiring drawing',
+  sDefaultContent: '',
+  mData: 'from.wiringDrawing',
+  bFilter: true
+}, {
+  sTitle: 'label',
+  sDefaultContent: '',
+  mData: 'from.label',
+  bFilter: true
+}];
+
+var toColumns = [{
+  sTitle: 'To building',
+  sDefaultContent: '',
+  mData: 'to.building',
+  bFilter: true
+}, {
+  sTitle: 'room',
+  sDefaultContent: '',
+  mData: 'to.room',
+  bFilter: true
+}, {
+  sTitle: 'elevation',
+  sDefaultContent: '',
+  mData: 'to.elevation',
+  bFilter: true
+}, {
+  sTitle: 'unit',
+  sDefaultContent: '',
+  mData: 'to.unit',
+  bFilter: true
+}, {
+  sTitle: 'term. device',
+  sDefaultContent: '',
+  mData: 'to.terminationDevice',
+  bFilter: true
+}, {
+  sTitle: 'term. type',
+  sDefaultContent: '',
+  mData: 'to.terminationType',
+  bFilter: true
+}, {
+  sTitle: 'wiring drawing',
+  sDefaultContent: '',
+  mData: 'to.wiringDrawing',
+  bFilter: true
+}, {
+  sTitle: 'label',
+  sDefaultContent: '',
+  mData: 'to.label',
+  bFilter: true
+}];
+
+var numberColumn = {
+  sTitle: 'Number',
+  mData: 'number',
+  mRender: function(data, type, full) {
+    return '<a href="cables/' + data + '" target="_blank">' + data + '</a>';
+  },
+  bFilter: true
+};
+
+var statusColumn = {
+  sTitle: 'Status',
+  // mData: 'status',
+  // mRender: function(data, type, full) {
+  //   return formatCableStatus(data);
+  // },
+  mData: function(source, type, val) {
+    return formatCableStatus(source.status);
+  },
+  bFilter: true
+};
+
+var oTableTools = {
+  "sSwfPath": "datatables/swf/copy_csv_xls_pdf.swf",
+  "aButtons": [
+    "copy",
+    "print", {
+      "sExtends": "collection",
+      "sButtonText": 'Save <span class="caret" />',
+      "aButtons": ["csv", "xls", "pdf"]
+    }
+  ]
+};
+
+var sDom = "<'row-fluid'<'span6'<'control-group'T>>><'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>";
+
+
+// table functions
+
+function addEvents() {
+  $('tbody').on('click', 'input.select-row', function(e) {
+    if ($(this).prop('checked')) {
+      $(e.target).closest('tr').addClass('row-selected');
+    } else {
+      $(e.target).closest('tr').removeClass('row-selected');
+    }
+  });
+
+  $('tfoot').on('keyup', 'input', function(e) {
+    var table = $(this).closest('table');
+    var th = $(this).closest('th');
+    table.dataTable().fnFilter(this.value, $('tfoot th', table).index(th));
+
+  });
 }
 
-function fnUnwrap(oTableLocal){
-  $(oTableLocal.fnSettings().aoData).each(function(){
-      $(this.nTr).addClass('nowrap');
-    });
-    oTableLocal.fnAdjustColumnSizing();
+function formatCableStatus(s) {
+  var status = ['approved', 'ordered', 'received', 'accepted', 'labeled', 'bench terminated', 'bench tested', 'pulled', 'field terminated', 'tested'];
+  if (0 <= s && s <= 9) {
+    return status[s];
+  }
+  return 'unknown';
+}
+
+
+function dateColumn(title, key) {
+  return {
+    sTitle: title,
+    mData: key,
+    sDefaultContent: '',
+    mRender: function(data, type, full) {
+      return formatDate(data);
+    },
+    sType: 'date'
+  };
+}
+
+function personColumn(title, key) {
+  return {
+    sTitle: title,
+    mData: key,
+    sDefaultContent: '',
+    mRender: function(data, type, full) {
+      return '<a href = "/users/' + data + '" target="_blank">' + data + '</a>';
+    },
+    bFilter: true
+  };
+}
+
+function createNullArray(size) {
+  var out = [];
+  for (var i = 0; i < size; i += 1) {
+    out.push(null);
+  }
+  return out;
+}
+
+
+
+function fnWrap(oTableLocal) {
+  $(oTableLocal.fnSettings().aoData).each(function() {
+    $(this.nTr).removeClass('nowrap');
+  });
+  oTableLocal.fnAdjustColumnSizing();
+}
+
+function fnUnwrap(oTableLocal) {
+  $(oTableLocal.fnSettings().aoData).each(function() {
+    $(this.nTr).addClass('nowrap');
+  });
+  oTableLocal.fnAdjustColumnSizing();
 }
 
 
@@ -30,10 +308,10 @@ function fnSelectAll(oTableLocal, selectedClass, checkboxClass, filtered) {
   fnDeselect(oTableLocal, selectedClass, checkboxClass);
   var settings = oTableLocal.fnSettings();
   var indexes = (filtered === true) ? settings.aiDisplay : settings.aiDisplayMaster;
-  indexes.forEach(function(i){
+  indexes.forEach(function(i) {
     var r = oTableLocal.fnGetNodes(i);
     $(r).addClass(selectedClass);
-    $(r).find('input.'+checkboxClass).prop('checked', true);
+    $(r).find('input.' + checkboxClass).prop('checked', true);
   });
 }
 
@@ -43,7 +321,7 @@ function fnDeselect(oTableLocal, selectedClass, checkboxClass) {
   for (var i = 0; i < aTrs.length; i++) {
     if ($(aTrs[i]).hasClass(selectedClass)) {
       $(aTrs[i]).removeClass(selectedClass);
-      $(aTrs[i]).find('input.'+checkboxClass+':checked').prop('checked', false);
+      $(aTrs[i]).find('input.' + checkboxClass + ':checked').prop('checked', false);
     }
   }
 }
@@ -57,9 +335,9 @@ function fnSetColumnsVis(oTableLocal, columns, show) {
 
 function fnAddFilterFoot(sTable, aoColumns) {
   var tr = $('<tr role="row">');
-  aoColumns.forEach(function(c){
-    if (c.bFilter){
-      tr.append('<th><input type="text" placeholder="'+c.sTitle+'" class="input-mini" autocomplete="off"></th>');
+  aoColumns.forEach(function(c) {
+    if (c.bFilter) {
+      tr.append('<th><input type="text" placeholder="' + c.sTitle + '" class="input-mini" autocomplete="off"></th>');
     } else {
       tr.append('<th></th>');
     }
@@ -76,4 +354,3 @@ $.fn.dataTableExt.afnSortData['dom-checkbox'] = function(oSettings, iColumn) {
     return $('td:eq(' + iColumn + ') input', tr).prop('checked') ? '1' : '0';
   });
 };
-
