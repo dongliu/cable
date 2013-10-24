e2db2b9c633fcffb89fee3e564eb2c97397a5f1e
Dong Liu <edongliu@gmail.com>
2013-10-24 13:32:00 -0400

    add srf rooms view

--- routes/room.js
+++ routes/room.js
@@ -2,15 +2,21 @@ var path = require('path');
 
 var frib = require('../config/fribroom.json');
 var nscl = require('../config/nscl.json');
-var building = {frib: frib, nscl: nscl};
+var srf = require('../config/srfroom.json');
+var building = {
+  frib: frib,
+  nscl: nscl,
+  srf: srf
+};
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.get('/:building/rooms', function(req, res) {
-    res.render('room', {json: path.join(req.path, '/json')});
+    res.render('room', {
+      json: path.join(req.path, '/json')
+    });
   });
 
   app.get('/:building/rooms/json', function(req, res) {
     res.json(building[req.params.building]);
   });
 };
-
