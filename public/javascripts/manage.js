a9b09f57c4f099f3cd3308aecd7bdda45f15cb5a
Dong Liu <edongliu@gmail.com>
2013-10-29 10:17:33 -0400

    update installing table column index

--- public/javascripts/manage.js
+++ public/javascripts/manage.js
@@ -10,7 +10,11 @@ var procuringTableColumns = {
   comments: [27]
 };
 
-var installingTableColumns = procuringTableColumns;
+var installingTableColumns = {
+  from: [12, 13, 14, 15, 16, 17, 18, 19],
+  to: [20, 21, 22, 23, 24, 25, 26, 27],
+  comments: [28]
+};
 
 var installedTableColumns = procuringTableColumns;
 
