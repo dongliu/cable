266bb8c328584aad2bebea57db0ad7c42aaadbd4
Dong Liu <edongliu@gmail.com>
2014-11-06 16:37:42 -0500

    update user route

--- routes/user.js
+++ routes/user.js
@@ -31,67 +31,111 @@ function addUser(req, res) {
       return res.send(400, req.body.name + ' is not unique!');
     }
 
+    var roles = [];
+    if (req.body.manager) {
+      roles.push('manager');
+    }
+    if (req.body.admin) {
+      roles.push('admin');
+    }
+
     var user = new User({
       adid: result[0].sAMAccountName,
       name: result[0].displayName,
       email: result[0].mail,
       office: result[0].physicalDeliveryOfficeName,
       phone: result[0].telephoneNumber,
-      mobile: result[0].mobile
+      mobile: result[0].mobile,
+      roles: roles
     });
 
     user.save(function (err, newUser) {
       if (err) {
-        console.error(err.msg);
-        return res.send(500, err.msg);
+        if (err.msg) {
+          console.error(err.msg);
+          return res.send(500, err.msg);
+        }
+        console.lod(err);
+        return res.send(500, 'cannot save the new user in db.');
       }
 
       var url = req.protocol + '://' + req.get('host') + '/users/' + newUser.adid;
       res.set('Location', url);
-      res.send(303, 'The new user is at ' + url);
+      res.send(303, 'The new user is at <a href="' + url + '">here</a>');
     });
 
   });
 }
 
+function updateUserProfile(user, res) {
+  var searchFilter = ad.searchFilter.replace('_id', user.adid);
+  var opts = {
+    filter: searchFilter,
+    attributes: ad.objAttributes,
+    scope: 'sub'
+  };
+  ldapClient.search(ad.searchBase, opts, false, function (err, result) {
+    if (err) {
+      return res.json(500, err);
+    }
+    if (result.length === 0) {
+      return res.json(500, {
+        error: user.adid + ' is not found!'
+      });
+    }
+    if (result.length > 1) {
+      return res.json(500, {
+        error: user.adid + ' is not unique!'
+      });
+    }
+    user.update({
+      name: result[0].displayName,
+      email: result[0].mail,
+      office: result[0].physicalDeliveryOfficeName,
+      phone: result[0].telephoneNumber,
+      mobile: result[0].mobile
+    }, function (err) {
+      if (err) {
+        return res.json(500, err);
+      }
+      res.send(204);
+    });
+  });
+}
+
 module.exports = function (app) {
 
-  app.get('/users', auth.ensureAuthenticated, function (req, res) {
-    if (req.query.name) {
-      User.findOne({
-        name: req.query.name
-      }).lean().exec(function (err, user) {
-        if (err) {
-          console.error(err.msg);
-          return res.send(500, err.msg);
-        }
-        if (user) {
-          return res.render('user', {
-            user: user,
-            myRoles: req.session.roles
-          });
-        }
-        return res.send(404, req.query.name + ' not found');
-      });
-    } else {
-      if (req.session.roles.length !== 0) {
-        return res.render('users');
-      }
+  app.get('/users/', auth.ensureAuthenticated, function (req, res) {
+    if (req.session.roles === undefined || req.session.roles.indexOf('admin') === -1) {
       return res.send(403, 'only admin allowed');
     }
-
+    return res.render('users');
   });
 
-  // app.get('/users/new', auth.ensureAuthenticated, function(req, res) {
-  //   res.render('user');
-  // });
+  app.get('/usernames/:name', auth.ensureAuthenticated, function (req, res) {
+    User.findOne({
+      name: req.params.name
+    }).lean().exec(function (err, user) {
+      if (err) {
+        console.error(err.msg);
+        return res.send(500, err.msg);
+      }
+      if (user) {
+        return res.render('user', {
+          user: user,
+          myRoles: req.session.roles
+        });
+      }
+      return res.send(404, req.params.name + ' not found');
+    });
+  });
 
-  app.post('/users', auth.ensureAuthenticated, function (req, res) {
+  app.post('/users/', auth.ensureAuthenticated, function (req, res) {
     // if (!req.is('json')) {
     //   return res.send(415, 'json request expected.');
     // }
 
-    if (req.session.roles.indexOf('admin') === -1) {
+    if (req.session.roles === undefined || req.session.roles.indexOf('admin') === -1) {
       return res.send(403, 'only admin allowed');
     }
 
@@ -99,10 +143,7 @@ module.exports = function (app) {
       return res.send(400, 'need to know name');
     }
 
-    // return res.send(200, req.body.name);
-
     // check if already in db
-
     User.findOne({
       name: req.body.name
     }).lean().exec(function (err, user) {
@@ -111,8 +152,7 @@ module.exports = function (app) {
       }
       if (user) {
         var url = req.protocol + '://' + req.get('host') + '/users/' + user.adid;
-        res.set('Location', url);
-        return res.send(303, 'The user is at ' + url);
+        return res.send(200, 'The user is at ' + url);
       }
       addUser(req, res);
     });
@@ -122,7 +162,7 @@ module.exports = function (app) {
 
 
   app.get('/users/json', auth.ensureAuthenticated, function (req, res) {
-    if (req.session.roles.length === 0) {
+    if (req.session.roles === undefined || req.session.roles.indexOf('admin') === -1) {
       return res.send(403, "You are not authorized to access this resource. ");
     }
     User.find().lean().exec(function (err, users) {
@@ -138,9 +178,6 @@ module.exports = function (app) {
 
 
   app.get('/users/:id', auth.ensureAuthenticated, function (req, res) {
-    // if (req.session.roles.indexOf('admin') === -1) {
-    //   return res.send(403, "You are not authorized to access this resource. ");
-    // }
     User.findOne({
       adid: req.params.id
     }).lean().exec(function (err, user) {
@@ -154,18 +191,16 @@ module.exports = function (app) {
           myRoles: req.session.roles
         });
       }
-      return res.send(404, req.params.id + ' not found');
+      return res.send(404, req.params.id + ' has never logged into the application.');
     });
   });
 
   app.put('/users/:id', auth.ensureAuthenticated, function (req, res) {
-    if (req.session.roles.indexOf('admin') === -1) {
+    if (req.session.roles === undefined || req.session.roles.indexOf('admin') === -1) {
       return res.send(403, "You are not authorized to access this resource. ");
     }
     if (!req.is('json')) {
-      return res.json(415, {
-        error: 'json request expected.'
-      });
+      return res.send(415, 'json request expected.');
     }
     User.findOneAndUpdate({
       adid: req.params.id
@@ -182,7 +217,6 @@ module.exports = function (app) {
 
   // get from the db not ad
   app.get('/users/:id/json', auth.ensureAuthenticated, function (req, res) {
-
     User.findOne({
       adid: req.params.id
     }).lean().exec(function (err, user) {
@@ -196,6 +230,24 @@ module.exports = function (app) {
     });
   });
 
+  app.get('/users/:id/refresh', auth.ensureAuthenticated, function (req, res) {
+    if (req.session.roles === undefined || req.session.roles.indexOf('admin') === -1) {
+      return res.send(403, "You are not authorized to access this resource. ");
+    }
+    User.findOne({
+      adid: req.params.id
+    }).exec(function (err, user) {
+      if (err) {
+        console.error(err.msg);
+        return res.send(500, err.msg);
+      }
+      if (user) {
+        updateUserProfile(user, res);
+      } else {
+        return res.send(404, req.params.id + ' is not in the application.');
+      }
+    });
+  });
 
 
   // resource /adusers
@@ -258,31 +310,28 @@ module.exports = function (app) {
   });
 
   app.get('/adusernames', auth.ensureAuthenticated, function (req, res) {
-    // var query = req.param('term');
     var query = req.query.term;
     var nameFilter, opts;
     if (query && query.length > 0) {
       nameFilter = ad.nameFilter.replace('_name', query + '*');
-      opts = {
-        filter: nameFilter,
-        attributes: ['displayName'],
-        scope: 'sub'
-      };
-      ldapClient.search(ad.searchBase, opts, false, function (err, result) {
-        if (err) {
-          return res.json(500, JSON.stringify(err));
-        }
-        if (result.length === 0) {
-          return res.json(500, {
-            error: 'Names starting with ' + query + ' are not found!'
-          });
-        }
-        return res.json(result);
-      });
     } else {
-      return res.json(500, {
-        error: 'query term is required.'
-      });
+      nameFilter = ad.nameFilter.replace('_name', '*');
     }
+    opts = {
+      filter: nameFilter,
+      attributes: ['displayName'],
+      scope: 'sub'
+    };
+    ldapClient.search(ad.searchBase, opts, false, function (err, result) {
+      if (err) {
+        return res.json(500, JSON.stringify(err));
+      }
+      if (result.length === 0) {
+        return res.json(500, {
+          error: 'Names starting with ' + query + ' are not found!'
+        });
+      }
+      return res.json(result);
+    });
   });
 };
