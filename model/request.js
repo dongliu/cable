a66b872703b16b134ec685c685e03e0025ef6cb2
Dong Liu <edongliu@gmail.com>
2014-11-21 14:45:35 -0500

    add more signal classification in model

--- model/request.js
+++ model/request.js
@@ -29,7 +29,7 @@ var request = new Schema({
     },
     signalClassification: {
       type: String,
-      enum: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K'],
+      enum: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M'],
       required: true
     },
     cableType: String,
