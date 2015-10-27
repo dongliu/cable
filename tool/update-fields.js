0f2944fc83d1f4bcf74974c8f52a7e85da20fe7a
Dong Liu <edongliu@gmail.com>
2015-10-27 17:21:36 -0400

    use commander for update field script

--- tool/update-fields.js
+++ tool/update-fields.js
@@ -1,65 +1,102 @@
+#!/usr/bin/env node
+
 /**
  * @fileOverview update a field according to a simple spec
  * @author Dong Liu
  */
+
+/*jslint es5: true*/
+
+var fs = require('fs');
+var path = require('path');
 
 var mongoose = require('mongoose');
 var Request = require('../model/request.js').Request;
 var Cable = require('../model/request.js').Cable;
 var Change = require('../model/request.js').Change;
 
-var choice;
+var program = require('commander');
+
+var inputPath;
+var realPath;
 var db;
-var spec = {
-  field_name: 'basic.cableType',
-  from_value: '2C_RG-59_Coax_035',
-  to_value: '2C_5kVDC_Coax_035'
-};
+var spec;
 var cablesProcessed = 0;
-var cablesToUpdate = 0;
 var cablesUpdated = 0;
-var cablesChecked = 0;
 var requestsProcessed = 0;
-var requestsToUpdate = 0;
 var requestsUpdated = 0;
-var requestsChecked = 0;
 
-var dryrun = true;
+program.version('0.0.1')
+  .option('-d, --dryrun', 'dryrun')
+  .option('-c, --cable', 'update cables according to the spec')
+  .option('-r, --request', 'update request according to the spec')
+  .arguments('<spec>')
+  .action(function (spec) {
+    inputPath = spec;
+  });
 
-if (process.argv.length === 3) {
-  choice = process.argv[2].toLowerCase();
-} else {
-  console.warn('needs exact one argument for cable|request');
+program.parse(process.argv);
+
+if (program.cable && program.request) {
+  console.log('please choose one option either -c or -r');
   process.exit(1);
 }
 
-if (['cable', 'request'].indexOf(choice) === -1) {
-  console.warn('Not a valid choice.');
+if (!program.cable && !program.request) {
+  console.log('please choose either -c or -r');
   process.exit(1);
 }
+
+if (inputPath === undefined) {
+  console.error('need the input json spec file path!');
+  process.exit(1);
+}
+
+realPath = path.resolve(process.cwd(), inputPath);
+
+if (!fs.existsSync(realPath)) {
+  console.error(realPath + ' does not exist.');
+  console.error('Please input a valid spec file path.');
+  process.exit(1);
+}
+
+spec = require(realPath);
+
+console.log(spec);
+
+if (program.cable) {
+  if (!spec.condition.status) {
+    spec.condition.status = {
+      $gte: 100,
+      $lte: 199
+    };
+  }
+}
+
+if (program.request) {
+  if (!spec.condition.status) {
+    spec.condition.status = 1;
+  }
+}
+
+console.log(spec);
 
 mongoose.connect('mongodb://localhost/cable_frib');
-
 db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function () {
   console.log('db connected');
 });
 
 function allDone() {
-  if (dryrun) {
+  if (program.dryrun) {
     console.log('Please note that this is a dryrun.');
   }
-  switch (choice) {
-  case 'request':
-    console.log(requestsChecked + ' requests were checked, ' + requestsToUpdate + ' requests need update, and ' + requestsUpdated + ' were updated.');
-    break;
-  case 'cable':
-    console.log(cablesChecked + ' cables were checked, ' + cablesToUpdate + ' cables need update, and ' + cablesUpdated + ' were updated.');
-    break;
-  default:
+  if (program.request) {
+    console.log(requestsUpdated + ' were updated.');
+  }
+  if (program.cable) {
+    console.log(cablesUpdated + ' were updated.');
   }
   mongoose.connection.close();
   console.log('Bye.');
@@ -76,45 +113,35 @@ function itemsAllChecked(total, processed, cb) {
 
 function checkRequests() {
   console.log('Starting processing requests ...');
-  Request.where('status').equals(1).exec(function (err, docs) {
+  Request.find(spec.condition).exec(function (err, docs) {
     var current = 0;
     if (err) {
       console.error(err);
     } else {
-      console.log('find ' + docs.length + ' requests to process in db.');
+      console.log('find ' + docs.length + ' requests to process for the condition.');
       docs.forEach(function (doc) {
-        console.log('checking ' + (++current) + ' request with id ' + doc._id);
-        var modified = 0;
+        console.log('processing ' + (++current) + ' request with id ' + doc._id);
         var update = {};
-        if (doc.get(spec.field_name) === spec.from_value) {
-          update[spec.field_name] = spec.to_value;
-          console.log('need to update request ' + doc._id + ' at ' + spec.field_name);
-          modified += 1;
-          requestsToUpdate += 1;
-        }
-        requestsChecked += 1;
-        if (dryrun) {
+        if (program.dryrun) {
           requestsProcessed += 1;
           itemsAllChecked(docs.length, requestsProcessed, allDone);
         } else {
-          if (modified > 0) {
-            update.updatedOn = Date.now();
-            update.updatedBy = 'system';
-            doc.update(update, {
-              new: true
-            }, function (err, cable) {
-              if (err) {
-                console.error(err);
-              } else {
-                requestsUpdated += 1;
-              }
-              requestsProcessed += 1;
-              itemsAllChecked(docs.length, requestsProcessed, allDone);
-            });
-          } else {
+          update.updatedOn = Date.now();
+          update.updatedBy = 'system';
+          update.$inc = {
+            __v: 1
+          };
+          doc.update(update, {
+            new: true
+          }, function (err, request) {
+            if (err) {
+              console.error(err);
+            } else {
+              requestsUpdated += 1;
+            }
             requestsProcessed += 1;
             itemsAllChecked(docs.length, requestsProcessed, allDone);
-          }
+          });
         }
       });
     }
@@ -123,34 +150,25 @@ function checkRequests() {
 
 function checkCables() {
   console.log('Starting processing cables ...');
-  Cable.where('status').gte(100).lte(199).exec(function (err, docs) {
+  Cable.find(spec.condition).exec(function (err, docs) {
     var current = 0;
     if (err) {
       console.error(err);
     } else {
-      console.log('find ' + docs.length + ' cables in db.');
-      docs.forEach(function (doc) {
-        console.log('checking ' + (++current) + ' cable with id ' + doc._id);
-        var update = {};
-        var change;
-        if (doc.get(spec.field_name) === spec.from_value) {
-          update[spec.field_name] = spec.to_value;
-          change = new Change({
-            cableName: doc._id,
-            property: spec.field_name,
-            oldValue: spec.from_value,
-            newValue: spec.to_value,
+      console.log('find ' + docs.length + ' cables for the condition.');
+      if (!program.dryrun) {
+        docs.forEach(function (doc) {
+          console.log('updating ' + (++current) + ' cable with id ' + doc._id);
+          var change = new Change({
+            cableName: doc.number,
+            property: spec.change.field_name,
+            oldValue: spec.change.from_value,
+            newValue: spec.change.to_value,
             updatedBy: 'system',
             updatedOn: Date.now()
           });
-          console.log('need to update cable ' + doc._id + ' at ' + spec.field_name);
-          cablesToUpdate += 1;
-        }
-        cablesChecked += 1;
-        if (dryrun) {
-          cablesProcessed += 1;
-          itemsAllChecked(docs.length, cablesProcessed, allDone);
-        } else {
+          var update = {};
+          update[spec.change.field_name] = spec.change.to_value;
           if (!!change) {
             update.updatedOn = Date.now();
             update.updatedBy = 'system';
@@ -183,18 +201,18 @@ function checkCables() {
             cablesProcessed += 1;
             itemsAllChecked(docs.length, cablesProcessed, allDone);
           }
-        }
-      });
+        });
+      }
     }
   });
 }
 
 
-if (choice === 'request') {
+if (program.request) {
   checkRequests();
 }
 
-if (choice === 'cable') {
+if (program.cable) {
   checkCables();
 }
 
