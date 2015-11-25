43ba37a10b0ec7af72f6ff0a1662bacc4dc9be31
Dong Liu <edongliu@gmail.com>
2015-11-25 14:58:00 -0500

    lint

--- public/javascripts/all-cable.js
+++ public/javascripts/all-cable.js
@@ -1,12 +1,8 @@
-/*global fnAddFilterFoot: false, typeColumns: false, sDom: false, oTableTools: false, filterEvent: false*/
+/*global fnAddFilterFoot: false, oTableTools: false, filterEvent: false, numberColumn: false, requestNumberColumn: false, statusColumn: false, versionColumn:falase, updatedOnLongColumn: false, approvedOnLongColumn: false, submittedByColumn:false, basicColumns: false, ownerProvidedColumn, fromColumns: false, toColumns: false,conduitColumn, lengthColumn, commentsColumn: false, sDom2InoF: false, fnAddFilterHead: false, fnWrap: false, fnUnwrap: false, barChart: false, selectEvent: false, highlightedEvent: false*/
+/*global ajax401: false*/
 /*global window: false*/
 $(function () {
-  $(document).ajaxError(function (event, jqxhr) {
-    if (jqxhr.status === 401) {
-      $('#message').append('<div class="alert alert-error"><button class="close" data-dismiss="alert">x</button>Please click <a href="/" target="_blank">home</a>, log in, and then save the changes on this page.</div>');
-      $(window).scrollTop($('#message div:last-child').offset().top - 40);
-    }
-  });
+  ajax401();
   var allAoColumns = [numberColumn, requestNumberColumn, statusColumn, versionColumn, updatedOnLongColumn, approvedOnLongColumn, submittedByColumn].concat(basicColumns.slice(0, 2), basicColumns.slice(3, 8), ownerProvidedColumn, fromColumns, toColumns).concat([conduitColumn, lengthColumn, commentsColumn]);
 
   var allTable = $('#all-cable').dataTable({
@@ -16,7 +12,7 @@ $(function () {
     iDisplayLength: 25,
     aLengthMenu: [
       [25, 50, 100, 500, 1000, -1],
-      [25, 50, 100, 500, 1000, "All"]
+      [25, 50, 100, 500, 1000, 'All']
     ],
     oLanguage: {
       sLoadingRecords: 'Please wait - loading data from the server ...'
@@ -28,29 +24,30 @@ $(function () {
       [5, 'desc'],
       [0, 'desc']
     ],
-    sDom: sDom2i1p,
+    sDom: sDom2InoF,
     oTableTools: oTableTools
   });
 
   fnAddFilterHead('#all-cable', allAoColumns);
   fnAddFilterFoot('#all-cable', allAoColumns);
 
-  $('#all-wrap').click(function (e) {
+  $('#all-wrap').click(function () {
     fnWrap(allTable);
   });
-  $('#all-unwrap').click(function (e) {
+  $('#all-unwrap').click(function () {
     fnUnwrap(allTable);
   });
 
-  $('#reload').click(function (e) {
+  $('#reload').click(function () {
     allTable.fnReloadAjax();
   });
 
-  $('#bar').click(function (e) {
+  $('#bar').click(function () {
     barChart(allTable);
   });
 
 
   filterEvent();
   selectEvent();
+  highlightedEvent();
 });
