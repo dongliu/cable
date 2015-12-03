2bddd4359883b09cebd82f2482c43cbffdd14bc8
Dong Liu <edongliu@gmail.com>
2015-12-03 15:57:27 -0500

    clone button for all tabs, enable delete, clean up

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,6 @@
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, FormData: false */
 /*global moment: false, ajax401: false, disableAjaxCache: false*/
-/*global selectColumn: false, editLinkColumn: false, detailsLinkColumn: false, createdOnColumn: false, rejectedOnColumn: false, rejectedByColumn: false, updatedOnColumn: false, submittedOnColumn: false, numberColumn: false, approvedOnColumn:false, approvedByColumn: false, fnAddFilterFoot: false, oTableTools: false, fnSelectAll: false, fnDeselect: false, basicColumns: false, fromColumns: false, toColumns: false, conduitColumn: false, lengthColumn: false, commentsColumn: false, statusColumn: false, ownerProvidedColumn: false, fnGetSelected: false, selectEvent: false, filterEvent: false, fnWrap: false, fnUnwrap: false, sDom2InoF: false, tabShownEvent: false, highlightedEvent: false*/
-
-var savedTable;
-var submittedTable;
-var rejectedTable;
-var approvedTable;
-var cablesTable;
+/*global selectColumn: false, editLinkColumn: false, detailsLinkColumn: false, createdOnColumn: false, rejectedOnColumn: false, rejectedByColumn: false, updatedOnColumn: false, submittedOnColumn: false, numberColumn: false, approvedOnColumn:false, approvedByColumn: false, fnAddFilterFoot: false, oTableTools: false, fnSelectAll: false, fnDeselect: false, basicColumns: false, fromColumns: false, toColumns: false, conduitColumn: false, lengthColumn: false, commentsColumn: false, statusColumn: false, ownerProvidedColumn: false, fnGetSelected: false, selectEvent: false, filterEvent: false, fnWrap: false, fnUnwrap: false, sDom2InoF: false, tabShownEvent: false, highlightedEvent: false, fnSetDeselect: false*/
 
 function initCable(table) {
   $.ajax({
@@ -28,7 +22,10 @@ function initCable(table) {
   });
 }
 
-function initRequests() {
+function initRequests(savedTable, submittedTable, rejectedTable, approvedTable, cablesTable) {
+  if (cablesTable) {
+    initCable(cablesTable);
+  }
   $.ajax({
     url: '/requests/json',
     type: 'GET',
@@ -100,19 +97,16 @@ function initRequests() {
       }
       approvedTable.fnDraw();
     }
-    if (cablesTable) {
-      initCable(cablesTable);
-    }
   }).fail(function () {
     $('#message').append('<div class="alert alert-error"><button class="close" data-dismiss="alert">x</button>Cannot reach the server for cable requests.</div>');
     $(window).scrollTop($('#message div:last-child').offset().top - 40);
-  }).always();
+  });
 }
 
-function submitFromModal() {
+function submitFromModal(rows, savedTable, submittedTable) {
   $('#submit').prop('disabled', true);
-  var number = $('#modal .modal-body div').length;
-  $('#modal .modal-body div').each(function () {
+  var number = $('#modal .modal-body .request').length;
+  $('#modal .modal-body .request').each(function (index) {
     var that = this;
     $.ajax({
       url: '/requests/' + that.id + '/',
@@ -124,6 +118,7 @@ function submitFromModal() {
     }).done(function () {
       $(that).prepend('<i class="icon-check"></i>');
       $(that).addClass('text-success');
+      fnSetDeselect(rows[index], 'row-selected', 'select-row');
     }).fail(function (jqXHR) {
       $(that).prepend('<i class="icon-question"></i>');
       $(that).append(' : ' + jqXHR.responseText);
@@ -137,19 +132,21 @@ function submitFromModal() {
   });
 }
 
-function batchSubmit(table) {
-  var selected = fnGetSelected(table, 'row-selected');
+function batchSubmit(savedTable, submittedTable) {
+  var selected = fnGetSelected(savedTable, 'row-selected');
+  var rows = [];
   if (selected.length) {
     $('#modalLabel').html('Submit the following ' + selected.length + ' requests for approval? ');
     $('#modal .modal-body').empty();
     selected.forEach(function (row) {
-      var data = table.fnGetData(row);
-      $('#modal .modal-body').append('<div id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.originCategory + data.basic.originSubcategory + data.basic.signalClassification + '||' + data.basic.wbs + '</div>');
+      rows.push(row);
+      var data = savedTable.fnGetData(row);
+      $('#modal .modal-body').append('<div class="request" id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.originCategory + data.basic.originSubcategory + data.basic.signalClassification + '||' + data.basic.wbs + '</div>');
     });
     $('#modal .modal-footer').html('<button id="submit" class="btn btn-primary">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
     $('#modal').modal('show');
     $('#submit').click(function () {
-      submitFromModal();
+      submitFromModal(rows, savedTable, submittedTable);
     });
   } else {
     $('#modalLabel').html('Alert');
@@ -159,45 +156,55 @@ function batchSubmit(table) {
   }
 }
 
-function cloneFromModal(requests) {
-  $('#clone').prop('disabled', true);
-  var number = $('#modal .modal-body div').length;
-  $('#modal .modal-body div').each(function () {
+function cloneFromModal(rows, requests, savedTable) {
+  $('#action').prop('disabled', true);
+  var number = $('#modal .modal-body .request').length;
+  $('#modal .modal-body .request').each(function (index) {
     var that = this;
-    $.ajax({
-      url: '/requests/',
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({
-        action: 'clone',
-        request: requests[that.id],
-        quantity: parseInt($('input', that).val(), 10)
-      })
-    }).done(function () {
-      $(that).prepend('<i class="icon-check"></i>');
-      $(that).addClass('text-success');
-    }).fail(function (jqXHR) {
+    var quantity = parseInt($('input', that).val(), 10);
+    if (!isNaN(quantity) && quantity > 0) {
+      $.ajax({
+        url: '/requests/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          action: 'clone',
+          request: requests[that.id],
+          quantity: quantity
+        })
+      }).done(function () {
+        $(that).prepend('<i class="icon-check"></i>');
+        $(that).addClass('text-success');
+        fnSetDeselect(rows[index], 'row-selected', 'select-row');
+      }).fail(function (jqXHR) {
+        $(that).prepend('<i class="icon-question"></i>');
+        $(that).append(' : ' + jqXHR.responseText);
+        $(that).addClass('text-error');
+      }).always(function () {
+        number = number - 1;
+        if (number === 0) {
+          initRequests(savedTable);
+        }
+      });
+    } else {
       $(that).prepend('<i class="icon-question"></i>');
-      $(that).append(' : ' + jqXHR.responseText);
+      $(that).append(' : the quantity is not acceptable');
       $(that).addClass('text-error');
-    }).always(function () {
-      number = number - 1;
-      if (number === 0) {
-        initRequests(savedTable);
-      }
-    });
+    }
   });
 }
 
-function batchClone(table) {
+function batchClone(table, savedTable) {
   var selected = fnGetSelected(table, 'row-selected');
   var requests = {};
+  var rows = [];
   if (selected.length) {
-    $('#modalLabel').html('Clone the following ' + selected.length + ' requests? ');
+    $('#modalLabel').html('Clone the following ' + selected.length + ' items? ');
     $('#modal .modal-body').empty();
     selected.forEach(function (row) {
+      rows.push(row);
       var data = table.fnGetData(row);
-      $('#modal .modal-body').append('<div id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.originCategory + data.basic.originSubcategory + data.basic.signalClassification + '||' + data.basic.wbs + ' <input type="text" placeholder="quantity" value="1" class="type[number] input-mini" min=1 max=20></div>');
+      $('#modal .modal-body').append('<div class="request" id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.originCategory + data.basic.originSubcategory + data.basic.signalClassification + '||' + data.basic.wbs + ' <input type="text" placeholder="quantity" value="1" class="type[number] input-mini" min=1 max=20></div>');
       requests[data._id] = {
         basic: data.basic,
         ownerProvided: data.ownerProvided,
@@ -208,10 +215,10 @@ function batchClone(table) {
         comments: data.comments
       };
     });
-    $('#modal .modal-footer').html('<button id="clone" class="btn btn-primary">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
+    $('#modal .modal-footer').html('<button id="action" class="btn btn-primary">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
     $('#modal').modal('show');
-    $('#clone').click(function () {
-      cloneFromModal(requests);
+    $('#action').click(function () {
+      cloneFromModal(rows, requests, savedTable);
     });
   } else {
     $('#modalLabel').html('Alert');
@@ -221,10 +228,10 @@ function batchClone(table) {
   }
 }
 
-function revertFromModal() {
+function revertFromModal(rows, savedTable, submittedTable) {
   $('#revert').prop('disabled', true);
-  var number = $('#modal .modal-body div').length;
-  $('#modal .modal-body div').each(function () {
+  var number = $('#modal .modal-body .request').length;
+  $('#modal .modal-body .request').each(function (index) {
     var that = this;
     $.ajax({
       url: '/requests/' + that.id + '/',
@@ -236,6 +243,7 @@ function revertFromModal() {
     }).done(function () {
       $(that).prepend('<i class="icon-check"></i>');
       $(that).addClass('text-success');
+      fnSetDeselect(rows[index], 'row-selected', 'select-row');
     }).fail(function (jqXHR) {
       $(that).prepend('<i class="icon-question"></i>');
       $(that).append(' : ' + jqXHR.responseText);
@@ -250,19 +258,21 @@ function revertFromModal() {
 }
 
 
-function batchRevert(table) {
-  var selected = fnGetSelected(table, 'row-selected');
+function batchRevert(savedTable, submittedTable) {
+  var selected = fnGetSelected(submittedTable, 'row-selected');
+  var rows = [];
   if (selected.length) {
     $('#modalLabel').html('Revert the following ' + selected.length + ' requests? ');
     $('#modal .modal-body').empty();
     selected.forEach(function (row) {
-      var data = table.fnGetData(row);
-      $('#modal .modal-body').append('<div id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.originCategory + data.basic.originSubcategory + data.basic.signalClassification + '||' + data.basic.wbs + '</div>');
+      rows.push(row);
+      var data = submittedTable.fnGetData(row);
+      $('#modal .modal-body').append('<div class="request" id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.originCategory + data.basic.originSubcategory + data.basic.signalClassification + '||' + data.basic.wbs + '</div>');
     });
     $('#modal .modal-footer').html('<button id="revert" class="btn btn-primary">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
     $('#modal').modal('show');
     $('#revert').click(function () {
-      revertFromModal();
+      revertFromModal(rows, savedTable, submittedTable);
     });
   } else {
     $('#modalLabel').html('Alert');
@@ -272,40 +282,41 @@ function batchRevert(table) {
   }
 }
 
-// function deleteFromModal() {
-//   $('#delete').prop('disabled', true);
-//   var number = $('#modal .modal-body div').length;
-//   $('#modal .modal-body div').each(function () {
-//     var that = this;
-//     $.ajax({
-//       url: '/requests/' + this.id + '/',
-//       type: 'Delete'
-//     }).done(function () {
-//       $(that).wrap('<del></del>');
-//       $(that).addClass('text-success');
-//     }).fail(function (jqXHR) {
-//       $(that).append(' : ' + jqXHR.responseText);
-//       $(that).addClass('text-error');
-//     }).always(function () {
-//       number = number - 1;
-//       if (number === 0) {
-//         initRequests(savedTable);
-//       }
-//     });
-//   });
-// }
+function deleteFromModal(table, rows) {
+  $('#delete').prop('disabled', true);
+  // var number = $('#modal .modal-body .request').length;
+  $('#modal .modal-body .request').each(function (index) {
+    var that = this;
+    $.ajax({
+      url: '/requests/' + this.id + '/',
+      type: 'Delete'
+    }).done(function () {
+      $(that).wrap('<del></del>');
+      $(that).addClass('text-success');
+      table.fnDeleteRow(rows[index]);
+    }).fail(function (jqXHR) {
+      $(that).append(' : ' + jqXHR.responseText);
+      $(that).addClass('text-error');
+    });
+  });
+}
 
 function batchDelete(table) {
   var selected = fnGetSelected(table, 'row-selected');
+  var rows = [];
   if (selected.length) {
     $('#modalLabel').html('Delete the following ' + selected.length + ' requests? ');
     $('#modal .modal-body').empty();
     selected.forEach(function (row) {
       var data = table.fnGetData(row);
-      $('#modal .modal-body').append('<div id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.originCategory + data.basic.originSubcategory + data.basic.signalClassification + '||' + data.basic.wbs + '</div>');
+      rows.push(row);
+      $('#modal .modal-body').append('<div class="request" id="' + data._id + '">' + moment(data.createdOn).format('YYYY-MM-DD HH:mm:ss') + '||' + data.basic.originCategory + data.basic.originSubcategory + data.basic.signalClassification + '||' + data.basic.wbs + '</div>');
     });
-    $('#modal .modal-footer').html('<button id="delete" class="btn btn-primary" onclick="deleteFromModal()">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
+    $('#modal .modal-footer').html('<button id="delete" class="btn btn-primary"">Confirm</button><button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
     $('#modal').modal('show');
+    $('#delete').click(function () {
+      deleteFromModal(table, rows);
+    });
   } else {
     $('#modalLabel').html('Alert');
     $('#modal .modal-body').html('No request has been selected!');
@@ -316,12 +327,15 @@ function batchDelete(table) {
 
 
 $(function () {
+  var savedTable;
+  var submittedTable;
+  var rejectedTable;
+  var approvedTable;
+  var cablesTable;
+
+
   ajax401('');
   disableAjaxCache();
-
-  $('#reload').click(function () {
-    initRequests(savedTable, submittedTable, rejectedTable, approvedTable, cablesTable);
-  });
 
   /*saved tab starts*/
   // add footer first
@@ -363,12 +377,9 @@ $(function () {
   });
 
   $('#saved-submit').click(function () {
-    batchSubmit(savedTable);
+    batchSubmit(savedTable, submittedTable);
   });
 
-  $('#saved-clone').click(function () {
-    batchClone(savedTable);
-  });
   /*saved tab ends*/
 
   /*submitted tab starts*/
@@ -397,12 +408,8 @@ $(function () {
     fnUnwrap(submittedTable);
   });
 
-  $('#submitted-clone').click(function () {
-    batchClone(submittedTable);
-  });
-
   $('#submitted-revert').click(function () {
-    batchRevert(submittedTable);
+    batchRevert(savedTable, submittedTable);
   });
 
   $('#submitted-select-all').click(function () {
@@ -453,10 +460,6 @@ $(function () {
     batchDelete(rejectedTable);
   });
 
-  $('#rejected-clone').click(function () {
-    batchClone(rejectedTable);
-  });
-
   /*rejected tab ends*/
 
   /*approved tab starts*/
@@ -492,14 +495,10 @@ $(function () {
     fnUnwrap(approvedTable);
   });
 
-  $('#approved-clone').click(function () {
-    batchClone(approvedTable);
-  });
-
   /*approved tab ends*/
 
   /*cables tab starts*/
-  var cableAoCulumns = [numberColumn, statusColumn, updatedOnColumn].concat(basicColumns.slice(0, 2), basicColumns.slice(3, 8), ownerProvidedColumn, fromColumns, toColumns).concat([conduitColumn, lengthColumn, commentsColumn]);
+  var cableAoCulumns = [selectColumn, numberColumn, statusColumn, updatedOnColumn].concat(basicColumns.slice(0, 2), basicColumns.slice(3, 8), ownerProvidedColumn, fromColumns, toColumns).concat([conduitColumn, lengthColumn, commentsColumn]);
 
   fnAddFilterFoot('#cables-table', cableAoCulumns);
   cablesTable = $('#cables-table').dataTable({
@@ -507,8 +506,8 @@ $(function () {
     bAutoWidth: false,
     aoColumns: cableAoCulumns,
     'aaSorting': [
-      [2, 'desc'],
-      [0, 'desc']
+      [3, 'desc'],
+      [1, 'desc']
     ],
     sDom: sDom2InoF,
     oTableTools: oTableTools,
@@ -527,6 +526,15 @@ $(function () {
   /*cables tab ends*/
 
   initRequests(savedTable, submittedTable, rejectedTable, approvedTable, cablesTable);
+
+  $('#reload').click(function () {
+    initRequests(savedTable, submittedTable, rejectedTable, approvedTable, cablesTable);
+  });
+
+  $('#clone').click(function cloneHandler() {
+    var activeTable = $($.fn.dataTable.fnTables(true)[0]).dataTable();
+    batchClone(activeTable, savedTable);
+  });
 
   tabShownEvent();
   selectEvent();
