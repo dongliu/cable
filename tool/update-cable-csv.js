3fef91e95fa76a10ef501839e300b66514965605
Dylan Maxwell <maxwelld@frib.msu.edu>
2017-02-14 13:41:57 -0500

    Update error messages for update-cable-csv tool

--- tool/update-cable-csv.js
+++ tool/update-cable-csv.js
@@ -34,21 +34,24 @@ program.version('0.0.1')
 program.parse(process.argv);
 
 if (inputPath === undefined) {
-  console.error('error: need the input source csv file path!');
+  console.error('Error: need the input source csv file path!');
   process.exit(1);
 }
 
 realPath = path.resolve(process.cwd(), inputPath);
 
 if (!fs.existsSync(realPath)) {
-  console.error('error: ' + realPath + ' does not exist.');
+  console.error('Error: ' + realPath + ' does not exist.');
   console.error('Please input a valid csv file path.');
   process.exit(1);
 }
 
 mongoose.connect('mongodb://localhost/cable_frib');
 db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', function(err) {
+  console.error(err);
+  process.exit(1);
+});
 db.once('open', function () {
   console.log('db connected');
 });
@@ -60,107 +63,107 @@ function splitTags(s) {
 function updateCable(change, i, callback) {
   console.log('processing change ' + i);
   Cable.findOne({ number: change[0] }).exec(function (err, cable) {
-      if (err) {
-        console.error('error: ' + err);
-        callback(err);
-        return;
+    if (err) {
+      console.error(err);
+      callback(err);
+      return;
+    }
+
+    if (!cable) {
+      err = new Error('cannot find cable with id ' + change[0]);
+      console.error(err);
+      callback(err);
+      return;
+    }
+
+    var update = {};
+    var updates = [];
+    var multiChange = null;
+    var conditionSatisfied = true;
+
+    properties.forEach(function (p, index) {
+      var currentValue = cable.get(p);
+      switch (p) {
+      case 'basic.tags':
+        currentValue = currentValue ? currentValue.join() : '';
+        break;
+      case 'status':
+        change[2 * index + 1] = parseInt(change[2 * index + 1]);
+        change[2 * index + 2] = parseInt(change[2 * index + 2]);
       }
 
-      if (!cable) {
-        err = new Error('cannot find cable with id ' + change[0]);
-        console.error('error: ' + err);
-        callback(err);
-        return;
-      }
-
-      var update = {};
-      var updates = [];
-      var multiChange = null;
-      var conditionSatisfied = true;
-
-      properties.forEach(function (p, index) {
-        var currentValue = cable.get(p);
-        switch (p) {
-        case 'basic.tags':
-          currentValue = currentValue ? currentValue.join() : '';
-          break;
-        case 'status':
-          change[2 * index + 1] = parseInt(change[2 * index + 1]);
-          change[2 * index + 2] = parseInt(change[2 * index + 2]);
-        }
-
-        if (change[2 * index + 1] === '_whatever_' || currentValue === change[2 * index + 1]) {
-          if (change[2 * index + 2] !== currentValue) {
-            if ([undefined, null, ''].indexOf(change[2 * index + 2]) !== -1 && [undefined, null, ''].indexOf(currentValue) !== -1) {
-              // do nothing
+      if (change[2 * index + 1] === '_whatever_' || currentValue === change[2 * index + 1]) {
+        if (change[2 * index + 2] !== currentValue) {
+          if ([undefined, null, ''].indexOf(change[2 * index + 2]) !== -1 && [undefined, null, ''].indexOf(currentValue) !== -1) {
+            // do nothing
+          } else {
+            if (p === 'basic.tags') {
+              update[p] = splitTags(change[2 * index + 2]);
             } else {
-              if (p === 'basic.tags') {
-                update[p] = splitTags(change[2 * index + 2]);
-              } else {
-                update[p] = change[2 * index + 2];
-              }
-              updates.push({
-                property: p,
-                oldValue: cable.get(p),
-                newValue: update[p]
-              });
+              update[p] = change[2 * index + 2];
             }
-          }
-        } else {
-          console.error('error: cable ' + cable.number + ' ' + p + ' is ' + cable.get(p) + ', expect ' + change[2 * index + 1]);
-          if (p === 'status') {
-            conditionSatisfied = false;
+            updates.push({
+              property: p,
+              oldValue: cable.get(p),
+              newValue: update[p]
+            });
           }
         }
-      });
+      } else {
+        console.error('Error: cable ' + cable.number + ' ' + p + ' is ' + cable.get(p) + ', expect ' + change[2 * index + 1]);
+        if (p === 'status') {
+          conditionSatisfied = false;
+        }
+      }
+    });
 
-      if (!(conditionSatisfied && updates.length > 0)) {
-        err = new Error(' no changes for cable ' + cable.number)
-        console.error('warning: ' + err);
-        callback(err);
+    if (!(conditionSatisfied && updates.length > 0)) {
+      err = new Error(' no changes for cable ' + cable.number)
+      console.error(err);
+      callback(err);
+      return;
+    }
+
+    update.updatedOn = Date.now();
+    update.updatedBy = 'system';
+    update.$inc = {
+      __v: 1
+    };
+
+    if (program.dryrun) {
+      console.log('cable ' + cable.number + ' will be updated with ' + JSON.stringify(update, null, 2));
+      callback(null);
+      return;
+    }
+
+    multiChange = new MultiChange({
+      cableName: cable.number,
+      updates: updates,
+      updatedBy: 'system',
+      updatedOn: Date.now()
+    });
+
+    multiChange.save(function (err1, c) {
+      if (err1) {
+        console.error(err1);
+        callback(err1);
         return;
       }
 
-        update.updatedOn = Date.now();
-        update.updatedBy = 'system';
-        update.$inc = {
-          __v: 1
-        };
+      update.$push = {
+        changeHistory: c._id
+      };
 
-        if (program.dryrun) {
-          console.log('cable ' + cable.number + ' will be updated with ' + JSON.stringify(update, null, 2));
-          callback(null);
+      cable.update(update, { new: true }, function (err2) {
+        if (err2) {
+          console.error(err2);
+          callback(err2);
           return;
         }
-
-          multiChange = new MultiChange({
-            cableName: cable.number,
-            updates: updates,
-            updatedBy: 'system',
-            updatedOn: Date.now()
-          });
-
-          multiChange.save(function (err1, c) {
-            if (err1) {
-              console.error('error: ' + err1);
-              callback(err1);
-              return;
-            }
-
-              update.$push = {
-                changeHistory: c._id
-              };
-
-              cable.update(update, { new: true }, function (err2) {
-                if (err2) {
-                  console.error('error: ' + err2);
-                  callback(err2);
-                  return;
-                }
-                console.log('cable ' + cable.number + ' was updated with ' + JSON.stringify(update, null, 2));
-                callback(null);
-              });
+        console.log('cable ' + cable.number + ' was updated with ' + JSON.stringify(update, null, 2));
+        callback(null);
       });
+    });
   });
 
 }
@@ -178,7 +181,7 @@ parser.on('readable', function () {
     console.log('read ' + line + ' lines ...');
     if (line === 1) {
       if (record[0] !== 'number') {
-        console.error('error: first column must be number');
+        console.error('Error: first column must be number');
         process.exit(1);
       }
       for (i = 1; i < record.length; i += 1) {
@@ -198,7 +201,7 @@ parser.on('readable', function () {
 });
 
 parser.on('error', function (err) {
-  console.error('error: ' + err.message);
+  console.error(err);
   process.exit(1);
 });
 
