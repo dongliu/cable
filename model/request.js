267b7dc3b51291daebf13e3a27af92f6d7a24e1f
Dylan Maxwell <maxwelld@frib.msu.edu>
2017-11-02 15:14:39 -0400

    Add new Project and Origin Category for SECAR facility

--- model/request.js
+++ model/request.js
@@ -6,8 +6,8 @@ var Mixed = Schema.Types.Mixed;
 var ObjectId = Schema.Types.ObjectId;
 
 // shared configuration for request and cable schemas
-var projectValues = ['FRIB', 'REA6'];
-var originCategoryValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A'];
+var projectValues = ['FRIB', 'REA6', 'SECAR'];
+var originCategoryValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B'];
 var originSubcategoryValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var signalClassificationValues = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N'];
 var traySectionValues = ['HPRF', 'DC', 'VLLS', 'LLS', 'HVDC', 'MLS', 'AC', 'MV-AC', 'REF', 'PPS', 'N/A'];
